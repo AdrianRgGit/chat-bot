b212385fcd842f4a3784e4f423a09c5e42d5a2b0
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-06-06 10:40:36 +0200

    añadida animación y ahora empieza desde abajo el chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          "0%": { opacity: 0, transform: "translateX(100%)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        slideIn: "slideIn 0.3s ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
